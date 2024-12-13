63bc09a95613dfc2a9456acd18c85c0e7d087001
carnasci <cen.master24@gmail.com>
2024-12-13 16:44:23 -0600

    Day 11 Blackjack capstone project with hints

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -69,7 +69,8 @@ from art import logo
 
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    return random.choice(cards)
+    card = random.choice(cards)
+    return card
 
 def calculate_score(cards):
 
