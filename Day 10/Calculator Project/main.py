a6a9252cfdd1d272f1ae713b90165e0abde2ec6e
carnasci <cen.master24@gmail.com>
2024-12-12 19:28:26 -0600

    Day 10 project task 3 finished

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,3 +1,7 @@
+from art import logo
+
+print(logo)
+
 def add(n1, n2):
     return n1 + n2
 
@@ -17,4 +21,23 @@ operations = {
     "/": div,
 }
 
-print(operations["*"](4, 8))
+#print(operations["*"](4, 8))
+calculate = True
+
+n1 = int(input("Please type your first number: "))
+while calculate:
+    o = input(" +\n -\n *\n /\n Which operator would you like to use?: ")
+
+    n2 = int(input("Please type your second number: "))
+
+    n3 = operations[o](n1, n2)
+
+    print(n3)
+    n1 = n3
+
+    again = input("Do you want to continue working working with the previous calculation?"
+              "type 'y' for yes, type 'n' for no.")
+
+    if again == "n":
+        calculate = False
+        n3 = 0
