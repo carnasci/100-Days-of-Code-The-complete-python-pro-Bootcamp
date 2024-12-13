0b7f9857768c1b139e9409357376dba6775c4ab4
carnasci <cen.master24@gmail.com>
2024-12-12 18:44:53 -0600

    Day 10 project task 2 finished

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,20 @@
 def add(n1, n2):
     return n1 + n2
 
+def sub(n1, n2):
+    return n1 - n2
 
+def mult(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": sub,
+    "*": mult,
+    "/": div,
+}
+
+print(operations["*"](4, 8))
