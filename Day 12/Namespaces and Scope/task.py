26146a5498441e0baefebc9ab11f1ed816eaab68
carnasci <cen.master24@gmail.com>
2024-12-14 18:11:47 -0600

    Day 12 is_prime() coding challenge finished

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -8,3 +8,21 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+
+def is_prime(num):
+    numbers = []
+    for i in range(1, (num + 1)):
+
+        if num % i == 0:
+            numbers.append(i)
+
+    if len(numbers) == 2:
+        return True
+    else:
+        return False
+
+
+print(is_prime(73))
+
+print(is_prime(75))
