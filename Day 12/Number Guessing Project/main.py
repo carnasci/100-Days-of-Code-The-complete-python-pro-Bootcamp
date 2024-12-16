efc031af14914e150c0d033f27632d7d4ec18f19
carnasci <cen.master24@gmail.com>
2024-12-15 18:43:05 -0600

    Day 12 Guess the number project after solutions

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -41,7 +41,45 @@ print(logo)
 # guess_the_number(difficulty)
 
 # After seeing solutions video
+EASY_LEVEL_ATTEMPTS = 10
+HARD_LEVEL_ATTEMPTS = 5
 
-print("Welcome to the number guessing game!")
-print("I'm thinking of a number between 1 and 100.")
-random_num = random.randint(1, 100)
+def set_difficulty():
+        level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+        if level == "easy":
+            return EASY_LEVEL_ATTEMPTS
+        else:
+            return HARD_LEVEL_ATTEMPTS
+
+def check_answer(user_guess, actual_answer, turns):
+    if user_guess > actual_answer:
+        print("Too high.")
+        return turns - 1
+    elif user_guess < actual_answer:
+        print("Too low")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {actual_answer}.")
+
+
+def game():
+    print("Welcome to the number guessing game!")
+    print("I'm thinking of a number between 1 and 100.")
+    random_num = random.randint(1, 100)
+    attempts = set_difficulty()
+
+    guess = 0
+    while guess != random_num:
+
+        print(f"You have {attempts} attempts remaining to guess the number.")
+
+        guess = int(input("Make a guess: "))
+        attempts = check_answer(guess, random_num, attempts)
+        if attempts == 0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif guess != random_num:
+            print("Guess again.")
+
+game()
+
