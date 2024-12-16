2efa7c08be04570eb9e05fd37d6aed724f257957
carnasci <cen.master24@gmail.com>
2024-12-16 17:56:43 -0600

    Day 13 task 1 finished

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# The function is looping in a range of numbers between 1 and 20
 # 2. When is the function meant to print "You got it"?
+# When i is 20
 # 3. What are your assumptions about the value of i?
+# That it will equal 20 at some point
