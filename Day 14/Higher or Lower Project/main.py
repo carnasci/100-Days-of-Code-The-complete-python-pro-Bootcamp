e54fd36b062dc2d216a5fcc0b7bec3d37d69676b
carnasci <cen.master24@gmail.com>
2024-12-17 19:58:18 -0600

    Day 14 initial choice display working with correct choice saving to option A

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -2,9 +2,6 @@ from art import logo, vs
 from game_data import data
 import random
 
-# option_a = {}
-# option_b = {}
-game_score = 0
 
 # Pick 2 random options from data and assign them to a and be and remove them from data.
 
@@ -13,13 +10,49 @@ def random_pick():
     data.remove(pick)
     return pick
 
+# def compare_choices(choice_a, choice_b):
+#     if choice_a["follower_count"] > choice_b["follower_count"]:
+#         return choice_a
+#     else:
+#         return choice_b
+
+def compare_choices(list_of_choices):
+    followers = 0
+    winner = {}
+    for item in list_of_choices:
+        if item["follower_count"] > followers:
+            followers = item["follower_count"]
+            winner = item
+    return winner
 
 def game():
+
+    play_again = True
+    game_score = 0
     option_a = random_pick()
-    option_b = random_pick()
-    print(logo)
-    print(f"Compare A: {option_a["name"]}, a {option_a["description"]}, from {option_a["country"]}.")
-    print(vs)
-    print(f"Compare B: {option_b["name"]}, a {option_b["description"]}, from {option_b["country"]}.")
+    while play_again:
+        options = []
+        option_b = random_pick()
+        options.append(option_a)
+        options.append(option_b)
+        print(logo)
+        print(f"Compare A: {option_a["name"]}, a {option_a["description"]}, from {option_a["country"]}.")
+        print(vs)
+        print(f"Compare B: {option_b["name"]}, a {option_b["description"]}, from {option_b["country"]}.")
+        user_pick = input("Which has more followers? Type 'A' or 'B': ").lower()
+        if user_pick == "a":
+            user_pick = option_a
+        else:
+            user_pick = option_b
+
+        winner = compare_choices(options)
+        if user_pick == winner:
+            option_a = user_pick
+            game_score += 1
+        else:
+            play_again = False
+
+
+
 
 game()
